bd1fff5f2b1b2b8d672e8d245b37c746f8676712
Axel Gatineau Philippe <axel.gatineau-philippe@etu.univ-amu.fr>
2018-02-23 11:35:48 +0100

    Ajout CSS

---
+++ js/connect.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    $('#form-connexion').submit(function () {
+        $.ajax({
+            url: $(this).attr('action'),
+            method: $(this).attr('method'),
+            data: $(this).serialize()
+        })
+            .done(function (data) {
+                console.log('data=' + data);
+                if (data.est_connecte === true) {
+                    window.location.reload(true);
+                }
+                else {
+                    console.log('Pas connecté');
+                }
+            })
+            .fail(erreurCritique);
+        return false;
+    });
+
+    $('#form-deconnexion').submit(function () {
+        $.ajax({
+            url: $(this).attr('action'),
+            method: $(this).attr('method'),
+            data: $(this).serialize()
+        })
+            .done(function (data) {
+                console.log('data=' + data);
+                if (data.est_connecte === false) {
+                    window.location.reload(true);
+                }
+                else {
+                    console.log('Pas connecté');
+                }
+            })
+            .fail(erreurCritique);
+        return false;
+    });
+
+    $.ajax({
+        'url': '/json/est_connecte.php'
+    })
+        .done(function (data){
+            if (data.est_connecte) {
+                $('#form-deconnexion').slideDown(1000);
+                $('#conteneur').slideDown(1000);
+            }
+            else {
+                $('#form-connexion').slideDown(1000);
+            }
+        }).fail(erreurCritique);
+
+
+});
--- js/main.js
+++ js/main.js
@@ -5,76 +5,7 @@
         $('body').html('Erreur critique<br/>');
     };
 
-    let surbrillance = function() {
-        $(this).css({'background-color': 'red'});
-    };
-
-    let css_commun = {
-        'display': 'inline-block',
-        'border': '1px solid black',
-        'min-width': '50px',
-        'min-height': '50px',
-        'margin': '2',
-        'padding': '2',
-        'text-align': 'center',
-        'font-family': 'Arial',
-        'font-size' : '45px'
-    };
 
 
-    $(document).ready(function () {
-        $('#form-connexion').submit(function () {
-            $.ajax({
-                url: $(this).attr('action'),
-                method: $(this).attr('method'),
-                data: $(this).serialize()
-            })
-                .done(function (data) {
-                    console.log('data=' + data);
-                    if (data.est_connecte === true) {
-                        window.location.reload(true);
-                    }
-                    else {
-                        console.log('Pas connecté');
-                    }
-                })
-                .fail(erreurCritique);
-            return false;
-        });
-
-        $('#form-deconnexion').submit(function () {
-            $.ajax({
-                url: $(this).attr('action'),
-                method: $(this).attr('method'),
-                data: $(this).serialize()
-            })
-                .done(function (data) {
-                    console.log('data=' + data);
-                    if (data.est_connecte === false) {
-                        window.location.reload(true);
-                    }
-                    else {
-                        console.log('Pas connecté');
-                    }
-                })
-                .fail(erreurCritique);
-            return false;
-        });
-
-        $.ajax({
-            'url': '/json/est_connecte.php'
-        })
-            .done(function (data){
-                if (data.est_connecte) {
-                    $('#form-deconnexion').slideDown(1000);
-                    $('#conteneur').slideDown(1000);
-                }
-                else {
-                    $('#form-connexion').slideDown(1000);
-                }
-            }).fail(erreurCritique);
-
-
-    });
 
 }) ();
