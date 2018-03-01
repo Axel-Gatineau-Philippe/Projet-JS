f81dd702618d7e174e3a4ebd0f449237249cfa17
Axel Gatineau Philippe <axel.gatineau-philippe@etu.univ-amu.fr>
2018-03-01 23:06:04 +0100

    Hide fonctionnel

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@
                 console.log('musiques=' + musiques);
                 for (let i=0; i<musiques.liste.length; ++i) {
                     let musique = musiques.liste[i];
-                    $('.musique').append(
+                    $('#musique').append(
                         $('<div />').append(musique.titre),
                         $('<div />').append(musique.artiste),
                         $('<div />').append(musique.date),
@@ -81,8 +81,11 @@
                 }
             }).fail(erreurCritique);
             return false;
-        }).click(function () {
-            $('.musique').toggleClass("hide", function () {
+        })/*.click(function () {
+            $('#musique').toggleClass("show", function () {
+            })
+        })*/.click(function () {
+            $('#musique').toggleClass("hide", function () {
             });
         });
     });
